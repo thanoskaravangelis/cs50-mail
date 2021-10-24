a6a65bc68e592e82f4b2b139a8584b5780c889ab
thanoskaravangelis <thanosblv@gmail.com>
2021-10-24 16:10:30 +0300

    Add models,views,urls etc

---
+++ mail/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from . import views
+from project3 import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+
+    # API Routes
+    path("emails", views.compose, name="compose"),
+    path("emails/<int:email_id>", views.email, name="email"),
+    path("emails/<str:mailbox>", views.mailbox, name="mailbox"),
+]
+
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
